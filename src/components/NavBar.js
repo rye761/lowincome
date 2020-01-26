ff806a3c327791a833543b03b0b9730cae74b40a
Ryan <ryan@mmnto.io>
2020-01-25 20:32:53 -0500

    Dashboard start

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -48,6 +48,17 @@ const NavBar = () => {
                   Home
                 </NavLink>
               </NavItem>
+              {isAuthenticated && (
+                <NavItem>
+                  <NavLink
+                    tag={RouterNavLink}
+                    to="/dashboard"
+                    exact
+                    activeClassName="router-link-exact-active"
+                  >Dashboard
+                  </NavLink>
+                </NavItem>
+              )}
             </Nav>
             <Nav className="d-none d-md-block" navbar>
               {!isAuthenticated && (
---
+++ src/views/Dashboard.js
@@ -0,0 +1,76 @@
+import React, { useState, useEffect } from "react";
+
+import Loading from "../components/Loading";
+import { useAuth0 } from "../react-auth0-spa";
+
+const DASHBOARD_ITEMS = [
+  {
+    name: "HSR Fare Subsidy",
+    description: "Low income families receive 50% lower fares on monthly passses with the HSR!",
+    price: 50.99
+  },
+  {
+    name: "HSR Fare Subsidy",
+    description: "Low income families receive 50% lower fares on monthly passses with the HSR!",
+    price: 50.99
+  },
+  {
+    name: "HSR Fare Subsidy",
+    description: "Low income families receive 50% lower fares on monthly passses with the HSR!",
+    price: 50.99
+  },
+  {
+    name: "HSR Fare Subsidy",
+    description: "Low income families receive 50% lower fares on monthly passses with the HSR!",
+    price: 50.99
+  },
+  {
+    name: "HSR Fare Subsidy",
+    description: "Low income families receive 50% lower fares on monthly passses with the HSR!",
+    price: 50.99
+  },
+  {
+    name: "HSR Fare Subsidy",
+    description: "Low income families receive 50% lower fares on monthly passses with the HSR!",
+    price: 50.99
+  },
+  {
+    name: "HSR Fare Subsidy",
+    description: "Low income families receive 50% lower fares on monthly passses with the HSR!",
+    price: 50.99
+  },
+  {
+    name: "HSR Fare Subsidy",
+    description: "Low income families receive 50% lower fares on monthly passses with the HSR!",
+    price: 50.99
+  }
+];
+
+const Profile = () => {
+  const { loading, user } = useAuth0();
+  const [ items, setItems ] = useState([]);
+
+  useEffect(() => {
+    setTimeout(() => setItems(DASHBOARD_ITEMS), 3000);
+  });
+
+  if (loading || !user || items.length == 0) {
+    return <Loading />;
+  }
+
+  const dashboardItems = items.map((item) => (
+    <div className="dashboard-item">
+      <h3>{ item.name }</h3>
+      <h4>{ item.description }</h4>
+      <h4>{ item.price }</h4>
+    </div>
+  ));
+
+  return (
+    <div className="dashboard-container">
+      { dashboardItems }
+    </div>
+  );
+};
+
+export default Profile;
